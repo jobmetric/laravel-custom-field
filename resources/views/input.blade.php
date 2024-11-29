20c10250fd8f2e824832d8ef256a8340076c7d02
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-29 21:38:57 +0330

    added view files

---
+++ resources/views/input.blade.php
@@ -0,0 +1,8 @@
+@php
+    echo '<pre dir="ltr">';
+    echo 'Field: '. $field->getName() . PHP_EOL;
+    var_dump($field);
+    echo '</pre>';
+    die;
+
+@endphp
---
+++ resources/views/select.blade.php
@@ -0,0 +1,7 @@
+@php
+    echo '<pre dir="ltr">';
+    var_dump($field);
+    echo '</pre>';
+    die;
+
+@endphp
