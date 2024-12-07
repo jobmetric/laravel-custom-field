36626a059172959014cbb5d150bfad7e3dc2460c
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-07 23:11:13 +0330

    added template select

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,7 +1,10 @@
-@php
-    echo '<pre dir="ltr">';
-    var_dump($field);
-    echo '</pre>';
-    die;
-
-@endphp
+<div class="mb-10">
+    <label class="form-label d-flex justify-content-between align-items-center">
+        <span class="required">{{ trans($field->getLabel()) }}</span>
+        <div class="text-gray-600 fs-7 d-none d-md-block d-lg-none d-xl-block">{{ trans($field->getInfo()) }}</div>
+    </label>
+    <select name="{{ $field->getName() }}"{!! $field->getAttributes() !!}>
+        {!! $field->getThemeOptions() !!}
+    </select>
+    <div class="text-gray-600 fs-7 mt-2 d-md-none d-lg-block d-xl-none">{{ trans($field->getInfo()) }}</div>
+</div>
