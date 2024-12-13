e4a29371d5eee0bd63a0201b01477a5aa2f0eec9
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-13 23:04:23 +0330

    added php doc in field contract

--- src/Fields/FieldContract.php
+++ src/Fields/FieldContract.php
@@ -2,14 +2,35 @@
 
 namespace JobMetric\CustomField\Fields;
 
+use Throwable;
+
 interface FieldContract
 {
     /**
      * render the field as HTML
      *
+     * @param array|string|int|bool|null $value
      * @param array $replaces
+     * @param bool $showInfo
+     * @param string $class
+     * @param string|null $classParent
+     * @param bool $hasErrorTagForm
+     * @param bool $hasErrorTagJs
+     * @param string|null $errorTagClass
+     * @param string|null $prefixId
      *
      * @return string
+     * @throws Throwable
      */
-    public function render(array $replaces = []): string;
+    public function render(
+        array|string|int|bool|null $value = null,
+        array                      $replaces = [],
+        bool                       $showInfo = true,
+        string                     $class = '',
+        string                     $classParent = null,
+        bool                       $hasErrorTagForm = true,
+        bool                       $hasErrorTagJs = true,
+        string|null                $errorTagClass = null,
+        string|null                $prefixId = null
+    ): string;
 }
