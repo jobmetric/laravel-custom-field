43456eb770f47fd4a164e38bdb281feb04bff6ba
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-20 16:35:37 +0330

    added base language file in fa key

---
+++ lang/fa/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Custom Field Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Custom Field for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'خطای اعتبارسنجی رخ داده است.',
+    ],
+
+    'exceptions' => [
+        'option_empty_label' => 'برچسب گزینه نمی تواند خالی باشد.',
+    ],
+
+    'messages' => [
+    ],
+
+];
