a5bc1505d88b1ea1080c4db00c0fa252522cd7cf
hassan7303 <hassanali7303@gmail.com>
2025-01-03 18:47:03 +0330

    add BuilderImage, BuilderTel, BuilderTime, and BuilderWeek to CustomFieldBuilder

--- src/CustomFieldBuilder.php
+++ src/CustomFieldBuilder.php
@@ -8,12 +8,16 @@ use JobMetric\CustomField\Fields\BuilderDate;
 use JobMetric\CustomField\Fields\BuilderDateTimeLocal;
 use JobMetric\CustomField\Fields\BuilderEmail;
 use JobMetric\CustomField\Fields\BuilderHidden;
+use JobMetric\CustomField\Fields\BuilderImage;
 use JobMetric\CustomField\Fields\BuilderMonth;
 use JobMetric\CustomField\Fields\BuilderNumber;
 use JobMetric\CustomField\Fields\BuilderPassword;
 use JobMetric\CustomField\Fields\BuilderRange;
 use JobMetric\CustomField\Fields\BuilderSelect;
+use JobMetric\CustomField\Fields\BuilderTel;
 use JobMetric\CustomField\Fields\BuilderText;
+use JobMetric\CustomField\Fields\BuilderTime;
+use JobMetric\CustomField\Fields\BuilderWeek;
 use JobMetric\CustomField\Fields\FieldContract;
 
 class CustomFieldBuilder
@@ -27,8 +31,12 @@ class CustomFieldBuilder
         BuilderDateTimeLocal,
         BuilderEmail,
         BuilderMonth,
+        BuilderTime,
         BuilderPassword,
         BuilderRange,
+        BuilderWeek,
+        BuilderTel,
+        BuilderImage,
         BuilderColor;
 
     public static FieldContract $fieldContract;
