6716ce546a32f34c50eb6fd4f417e02f59bd3069
hassan7303 <hassanali7303@gmail.com>
2025-01-03 18:46:55 +0330

    add Image, Tel, Time, and Week fields to FieldFactory

--- src/FieldFactory.php
+++ src/FieldFactory.php
@@ -5,6 +5,7 @@ namespace JobMetric\CustomField;
 use Exception;
 use JobMetric\CustomField\Fields\Color;
 use JobMetric\CustomField\Fields\Date;
+use JobMetric\CustomField\Fields\Image;
 use JobMetric\CustomField\Fields\Password;
 use JobMetric\CustomField\Fields\DateTimeLocal;
 use JobMetric\CustomField\Fields\Email;
@@ -14,7 +15,10 @@ use JobMetric\CustomField\Fields\Month;
 use JobMetric\CustomField\Fields\Number;
 use JobMetric\CustomField\Fields\Range;
 use JobMetric\CustomField\Fields\Select;
+use JobMetric\CustomField\Fields\Tel;
 use JobMetric\CustomField\Fields\Text;
+use JobMetric\CustomField\Fields\Time;
+use JobMetric\CustomField\Fields\Week;
 
 
 class FieldFactory
@@ -38,8 +42,12 @@ class FieldFactory
             'color'=> new Color,
             'month'=> new Month,
             'range'=> new Range,
+            'week'=> new Week,
+            'time'=> new Time,
             'password'=> new Password,
             'email'=> new Email,
+            'tel'=> new Tel,
+            'image'=> new Image,
             default => throw new Exception("Unsupported field type: $type"),
         };
     }
