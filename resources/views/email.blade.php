264038081bcb21e0701ae72209278a8482db7461
hassan7303 <hassanali7303@gmail.com>
2025-01-06 23:57:54 +0330

    feat: update email input not complited

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -9,7 +9,11 @@
             <div class="text-gray-600 fs-7 d-none d-md-block d-lg-none d-xl-block">{!! trans($field->getInfo()) !!}</div>
         @endif
     </label>
-    <input type="email" name="{{ $field->getName() }}"{!! $field->getAttributeTheme() !!}{!! $field->getThemeData() !!}>
+    <!-- <input type="email" name="{{ $field->getName() }}"{!! $field->getAttributeTheme() !!}{!! $field->getThemeData() !!}> -->
+    <div class="input-group input-group-solid mb-5">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+        <span class="input-group-text" id="basic-addon2">@example.com</span>
+    </div>
     @if($showInfo)
         <div class="text-gray-600 fs-7 mt-2 d-md-none d-lg-block d-xl-none">{!! trans($field->getInfo()) !!}</div>
     @endif
