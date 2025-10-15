cdfbbb24e82917eb97384055c8b87610cbfd67f5
majidmohammadian <majeedmohammadian@gmail.com>
2025-10-15 21:07:33 +0330

    removed view dependency in service provider

--- src/CustomFieldServiceProvider.php
+++ src/CustomFieldServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace JobMetric\CustomField;
 
-use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
 
@@ -12,12 +11,10 @@ class CustomFieldServiceProvider extends PackageCoreServiceProvider
      * @param PackageCore $package
      *
      * @return void
-     * @throws ViewFolderNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
         $package->name('laravel-custom-field')
-            ->hasTranslation()
-            ->hasView();
+            ->hasTranslation();
     }
 }
