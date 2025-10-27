7ec8f15fee06a391dee9685259baea1d1e53e062
majidmohammadian <majeedmohammadian@gmail.com>
2025-10-27 23:03:10 +0330

    added new example custom field

---
+++ src/CustomFields/Text/Text.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace JobMetric\CustomField\CustomFields\Text;
+
+use JobMetric\CustomField\Attribute\HasPlaceholder;
+use JobMetric\CustomField\Contracts\FieldContract;
+use JobMetric\CustomField\Core\BaseCustomField;
+
+class Text extends BaseCustomField implements FieldContract
+{
+    use HasPlaceholder;
+
+    /**
+     * The scripts to be included for the custom field.
+     *
+     * @var array
+     */
+    public array $scripts = [
+        'script.js',
+    ];
+
+    /**
+     * The styles to be included for the custom field.
+     *
+     * @var array
+     */
+    public array $styles = [
+        'style.css',
+    ];
+
+    /**
+     * Get the alias of the field.
+     *
+     * @return string
+     */
+    public static function alias(): string
+    {
+        return 'text';
+    }
+}
