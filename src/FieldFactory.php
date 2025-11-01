134648cf30fdb085ccbaa9118e2460e6915771a3
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-01 19:38:24 +0330

    changed FieldFactory for load custom field registry

--- src/FieldFactory.php
+++ src/FieldFactory.php
@@ -3,23 +3,9 @@
 namespace JobMetric\CustomField;
 
 use Exception;
+use InvalidArgumentException;
 use JobMetric\CustomField\Contracts\FieldContract;
-use JobMetric\CustomField\Fields\Color;
-use JobMetric\CustomField\Fields\Date;
-use JobMetric\CustomField\Fields\DateTimeLocal;
-use JobMetric\CustomField\Fields\Email;
-use JobMetric\CustomField\Fields\Hidden;
-use JobMetric\CustomField\Fields\Image;
-use JobMetric\CustomField\Fields\Month;
-use JobMetric\CustomField\Fields\Number;
-use JobMetric\CustomField\Fields\Password;
-use JobMetric\CustomField\Fields\Radio;
-use JobMetric\CustomField\Fields\Range;
-use JobMetric\CustomField\Fields\Select;
-use JobMetric\CustomField\Fields\Tel;
-use JobMetric\CustomField\Fields\Text;
-use JobMetric\CustomField\Fields\Time;
-use JobMetric\CustomField\Fields\Week;
+use JobMetric\CustomField\Support\CustomFieldRegistry;
 
 class FieldFactory
 {
@@ -27,29 +13,22 @@ class FieldFactory
      * Create a field instance based on the type
      *
      * @param string $type
+     *
      * @return FieldContract
      * @throws Exception
      */
     public static function create(string $type): FieldContract
     {
-        return match ($type) {
-            'number' => new Number,
-            'text' => new Text,
-            'select' => new Select,
-            'hidden' => new Hidden,
-            'date' => new Date,
-            'datetime-local' => new DateTimeLocal,
-            'color' => new Color,
-            'month' => new Month,
-            'range' => new Range,
-            'week' => new Week,
-            'time' => new Time,
-            'password' => new Password,
-            'email' => new Email,
-            'tel' => new Tel,
-            'radioAndCheckbox' => new Radio,
-            'image' => new Image,
-            default => throw new Exception("Unsupported field type: $type"),
-        };
+        /** @var CustomFieldRegistry $registry */
+        $registry = app('CustomFieldRegistry');
+
+        try {
+            $prototype = $registry->get($type);
+        } catch (InvalidArgumentException $e) {
+            throw new Exception("Unsupported field type: $type");
+        }
+
+        // Return a fresh clone to avoid shared state between usages
+        return clone $prototype;
     }
 }
